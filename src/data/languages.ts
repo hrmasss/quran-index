55223d05ba00edaea1dc187f564454608e26f8ed
Hojayfa Rahman <hojayfarahman@gmail.com>
2024-09-30 18:48:16 +0600

    add data to languages section

---
+++ src/data/languages.ts
@@ -0,0 +1,323 @@
+export interface Language {
+    name: string;
+    code: string;
+    flag: string;
+}
+
+export const languages: Language[] = [
+    {
+        name: "Arabic",
+        code: "ar",
+        flag: "https://flagcdn.com/h60/sa.webp",
+    },
+    {
+        name: "Bengali",
+        code: "bn",
+        flag: "https://flagcdn.com/h60/bd.webp",
+    },
+    {
+        name: "Chinese",
+        code: "zh",
+        flag: "https://flagcdn.com/h60/cn.webp",
+    },
+    {
+        name: "English",
+        code: "en",
+        flag: "https://flagcdn.com/h60/us.webp",
+    },
+    {
+        name: "French",
+        code: "fr",
+        flag: "https://flagcdn.com/h60/fr.webp",
+    },
+    {
+        name: "German",
+        code: "de",
+        flag: "https://flagcdn.com/h60/de.webp",
+    },
+    {
+        name: "Hindi",
+        code: "hi",
+        flag: "https://flagcdn.com/h60/in.webp",
+    },
+    {
+        name: "Indonesian",
+        code: "id",
+        flag: "https://flagcdn.com/h60/id.webp",
+    },
+    {
+        name: "Italian",
+        code: "it",
+        flag: "https://flagcdn.com/h60/it.webp",
+    },
+    {
+        name: "Japanese",
+        code: "ja",
+        flag: "https://flagcdn.com/h60/jp.webp",
+    },
+    {
+        name: "Korean",
+        code: "ko",
+        flag: "https://flagcdn.com/h60/kr.webp",
+    },
+    {
+        name: "Malay",
+        code: "ms",
+        flag: "https://flagcdn.com/h60/my.webp",
+    },
+    {
+        name: "Portuguese",
+        code: "pt",
+        flag: "https://flagcdn.com/h60/pt.webp",
+    },
+    {
+        name: "Russian",
+        code: "ru",
+        flag: "https://flagcdn.com/h60/ru.webp",
+    },
+    {
+        name: "Spanish",
+        code: "es",
+        flag: "https://flagcdn.com/h60/es.webp",
+    },
+    {
+        name: "Swahili",
+        code: "sw",
+        flag: "https://flagcdn.com/h60/ke.webp",
+    },
+    {
+        name: "Turkish",
+        code: "tr",
+        flag: "https://flagcdn.com/h60/tr.webp",
+    },
+    {
+        name: "Urdu",
+        code: "ur",
+        flag: "https://flagcdn.com/h60/pk.webp",
+    },
+    {
+        name: "Vietnamese",
+        code: "vi",
+        flag: "https://flagcdn.com/h60/vn.webp",
+    },
+    {
+        name: "Dutch",
+        code: "nl",
+        flag: "https://flagcdn.com/h60/nl.webp",
+    },
+    {
+        name: "Greek",
+        code: "el",
+        flag: "https://flagcdn.com/h60/gr.webp",
+    },
+    {
+        name: "Hebrew",
+        code: "he",
+        flag: "https://flagcdn.com/h60/il.webp",
+    },
+    {
+        name: "Thai",
+        code: "th",
+        flag: "https://flagcdn.com/h60/th.webp",
+    },
+    {
+        name: "Polish",
+        code: "pl",
+        flag: "https://flagcdn.com/h60/pl.webp",
+    },
+    {
+        name: "Czech",
+        code: "cs",
+        flag: "https://flagcdn.com/h60/cz.webp",
+    },
+    {
+        name: "Romanian",
+        code: "ro",
+        flag: "https://flagcdn.com/h60/ro.webp",
+    },
+    {
+        name: "Hungarian",
+        code: "hu",
+        flag: "https://flagcdn.com/h60/hu.webp",
+    },
+    {
+        name: "Finnish",
+        code: "fi",
+        flag: "https://flagcdn.com/h60/fi.webp",
+    },
+    {
+        name: "Norwegian",
+        code: "no",
+        flag: "https://flagcdn.com/h60/no.webp",
+    },
+    {
+        name: "Danish",
+        code: "da",
+        flag: "https://flagcdn.com/h60/dk.webp",
+    },
+    {
+        name: "Swedish",
+        code: "sv",
+        flag: "https://flagcdn.com/h60/se.webp",
+    },
+    {
+        name: "Bulgarian",
+        code: "bg",
+        flag: "https://flagcdn.com/h60/bg.webp",
+    },
+    {
+        name: "Catalan",
+        code: "ca",
+        flag: "https://flagcdn.com/h60/es.webp",
+    },
+    {
+        name: "Serbian",
+        code: "sr",
+        flag: "https://flagcdn.com/h60/rs.webp",
+    },
+    {
+        name: "Croatian",
+        code: "hr",
+        flag: "https://flagcdn.com/h60/hr.webp",
+    },
+    {
+        name: "Ukrainian",
+        code: "uk",
+        flag: "https://flagcdn.com/h60/ua.webp",
+    },
+    {
+        name: "Persian",
+        code: "fa",
+        flag: "https://flagcdn.com/h60/ir.webp",
+    },
+    {
+        name: "Malayalam",
+        code: "ml",
+        flag: "https://flagcdn.com/h60/in.webp",
+    },
+    {
+        name: "Tamil",
+        code: "ta",
+        flag: "https://flagcdn.com/h60/in.webp",
+    },
+    {
+        name: "Telugu",
+        code: "te",
+        flag: "https://flagcdn.com/h60/in.webp",
+    },
+    {
+        name: "Gujarati",
+        code: "gu",
+        flag: "https://flagcdn.com/h60/in.webp",
+    },
+    {
+        name: "Marathi",
+        code: "mr",
+        flag: "https://flagcdn.com/h60/in.webp",
+    },
+    {
+        name: "Punjabi",
+        code: "pa",
+        flag: "https://flagcdn.com/h60/in.webp",
+    },
+    {
+        name: "Thai",
+        code: "th",
+        flag: "https://flagcdn.com/h60/th.webp",
+    },
+    {
+        name: "Indonesian",
+        code: "id",
+        flag: "https://flagcdn.com/h60/id.webp",
+    },
+    {
+        name: "Swedish",
+        code: "sv",
+        flag: "https://flagcdn.com/h60/se.webp",
+    },
+    {
+        name: "Filipino",
+        code: "tl",
+        flag: "https://flagcdn.com/h60/ph.webp",
+    },
+    {
+        name: "Malay",
+        code: "ms",
+        flag: "https://flagcdn.com/h60/my.webp",
+    },
+    {
+        name: "Basque",
+        code: "eu",
+        flag: "https://flagcdn.com/h60/es.webp",
+    },
+    {
+        name: "Latvian",
+        code: "lv",
+        flag: "https://flagcdn.com/h60/lv.webp",
+    },
+    {
+        name: "Lithuanian",
+        code: "lt",
+        flag: "https://flagcdn.com/h60/lt.webp",
+    },
+    {
+        name: "Slovak",
+        code: "sk",
+        flag: "https://flagcdn.com/h60/sk.webp",
+    },
+    {
+        name: "Slovenian",
+        code: "sl",
+        flag: "https://flagcdn.com/h60/si.webp",
+    },
+    {
+        name: "Estonian",
+        code: "et",
+        flag: "https://flagcdn.com/h60/ee.webp",
+    },
+    {
+        name: "Icelandic",
+        code: "is",
+        flag: "https://flagcdn.com/h60/is.webp",
+    },
+    {
+        name: "Maltese",
+        code: "mt",
+        flag: "https://flagcdn.com/h60/mt.webp",
+    },
+    {
+        name: "Bosnian",
+        code: "bs",
+        flag: "https://flagcdn.com/h60/ba.webp",
+    },
+    {
+        name: "Khmer",
+        code: "km",
+        flag: "https://flagcdn.com/h60/kh.webp",
+    },
+    {
+        name: "Georgian",
+        code: "ka",
+        flag: "https://flagcdn.com/h60/ge.webp",
+    },
+    {
+        name: "Armenian",
+        code: "hy",
+        flag: "https://flagcdn.com/h60/am.webp",
+    },
+    {
+        name: "Azerbaijani",
+        code: "az",
+        flag: "https://flagcdn.com/h60/az.webp",
+    },
+    {
+        name: "Nepali",
+        code: "ne",
+        flag: "https://flagcdn.com/h60/np.webp",
+    },
+    {
+        name: "Sinhala",
+        code: "si",
+        flag: "https://flagcdn.com/h60/lk.webp",
+    },
+];
